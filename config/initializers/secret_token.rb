2b182ad6180e2eb5068f37ff83d3330b00e7fddd
Phill Conrad <pconrad@cs.ucsb.edu>
2015-09-17 16:25:20 -0700

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GmailAlerts::Application.config.secret_key_base = '3f175165be4192d5e22831b722ee6707ae2859c185fa5245a41cac99f6236f02fd549d06e44702ebdcd7a855659f7dca10ef48a08eec90b23da5bc1039a9a32a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GmailAlerts::Application.config.session_store :cookie_store, key: '_gmail-alerts_session'
