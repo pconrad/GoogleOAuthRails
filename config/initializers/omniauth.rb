1f451cfed6dbab68bcce7ab51c3d23becbe0d8c8
Phill Conrad <pconrad@cs.ucsb.edu>
2015-09-17 17:06:08 -0700

    add GOOGLE to env vars

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 #config/initalizers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
                              scope: ['email',
                                      'https://www.googleapis.com/auth/gmail.modify'],
                              access_type: 'offline'}
